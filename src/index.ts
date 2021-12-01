8aaf97927f7846003db377026914ed3d23357511
Dwi Andhika <dwiandhikaaji@gmail.com>
2021-12-01 10:37:57 +0700

    removed `svg.js` module

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import * as _ from "lodash";
+import { Replay, ReplayManipulator } from "./util/osu/ReplayManipulator";
+import * as $ from "jQuery";
 
-function component() {
-    const element = document.createElement("div");
+let replay = new Replay();
 
-    element.innerHTML = _.join(["Hello", "nuts"], " ");
+$("input:file").on("change", function () {
+    let reader = new FileReader();
+    reader.onload = async function () {
+        let arrayBuffer = this.result as ArrayBuffer;
+        let parser = new ReplayManipulator();
+        replay = await parser.parseFromBytes(arrayBuffer);
+    };
 
-    return element;
-}
+    console.log(typeof $(this).prop("files"));
+    reader.readAsArrayBuffer($(this).prop("files")[0]);
+});
 
-document.body.appendChild(component());
+$("button#replay-download").on("click", async function () {
+    const parser = new ReplayManipulator();
+    console.log(replay.getReplayData());
+    console.log(replay.getMods());
+
+    //    parser.saveReplayFile(replay, "pog.osr");
+});
