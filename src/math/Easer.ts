d848380ff2389c012e3694addd18a507236a4ba6
Dwi Andhika <dwiandhikaaji@gmail.com>
2022-05-11 08:17:20 +0700

    improved Easer code
    - SESE style on getValueAt method
    - can add and remove easing

--- src/math/Easer.ts
+++ src/math/Easer.ts
@@ -1,19 +1,29 @@
-import { EasingFunction, EasingType } from "./Easing";
+import { EasingFunction, EasingType } from "./EasingFunction";
 
-interface IEasing {
+interface Easing {
     startTime: number;
     endTime: number;
     targetFrom: number;
     targetTo: number;
-    easing: EasingType;
+    easingType: EasingType;
 }
 
 // Holds possibly different values depending on the given time
 class Easer {
-    private easings: IEasing[] = [];
+    easings: Easing[] = [];
     public time: number = 0;
 
-    constructor(public defaultValue?: number) {}
+    constructor(public defaultValue: number | undefined = 0) {}
+
+    static CreateEasing(
+        startTime: number,
+        endTime: number,
+        targetFrom: number,
+        targetTo: number,
+        easingType: EasingType = "Linear"
+    ): Easing {
+        return { startTime, endTime, targetFrom, targetTo, easingType };
+    }
 
     get value(): number {
         return this.getValueAt(this.time);
@@ -24,60 +34,87 @@ class Easer {
             return this.defaultValue ?? NaN;
         }
 
-        let result;
+        let result: number | undefined = undefined;
+        let selectedEasing: Easing | undefined = undefined;
 
         for (let i = this.easings.length - 1; i >= 0; i--) {
-            const transform = this.easings[i];
+            selectedEasing = this.easings[i];
 
-            if (time > transform.endTime || time < transform.startTime) {
+            if (time > selectedEasing.endTime || time < selectedEasing.startTime) {
                 continue;
             }
 
-            const t = (time - transform.startTime) / (transform.endTime - transform.startTime);
-            result = EasingFunction[transform.easing](t) * (transform.targetTo - transform.targetFrom) + transform.targetFrom;
+            const t = (time - selectedEasing.startTime) / (selectedEasing.endTime - selectedEasing.startTime);
+            result =
+                EasingFunction[selectedEasing.easingType](t) * (selectedEasing.targetTo - selectedEasing.targetFrom) +
+                selectedEasing.targetFrom;
             break;
         }
 
         if (result === undefined) {
             if (this.defaultValue !== undefined) {
-                return this.defaultValue;
-            }
+                result = this.defaultValue;
+            } else {
+                let minDeltaTime = Infinity;
+                let closestIndex;
+                for (let i = this.easings.length - 1; i >= 0; i--) {
+                    const transform = this.easings[i];
 
-            let minDeltaTime = Infinity;
-            let closestIndex;
-            for (let i = this.easings.length - 1; i >= 0; i--) {
-                const transform = this.easings[i];
+                    if (time < transform.endTime) continue;
 
-                if (time < transform.endTime) continue;
+                    const delta = time - transform.endTime;
+                    if (delta < minDeltaTime) {
+                        minDeltaTime = delta;
+                        closestIndex = i;
+                    }
+                }
 
-                const delta = time - transform.endTime;
-                if (delta < minDeltaTime) {
-                    minDeltaTime = delta;
-                    closestIndex = i;
+                if (closestIndex === undefined) {
+                    selectedEasing = this.easings[0];
+                    result = selectedEasing.targetFrom;
+                } else {
+                    selectedEasing = this.easings[closestIndex];
+                    result = selectedEasing.targetTo;
                 }
             }
-
-            if (closestIndex === undefined) {
-                return this.easings[0].targetFrom;
-            }
-
-            return this.easings[closestIndex].targetTo;
         }
 
         return result;
     }
 
-    addEasing(startTime: number, endTime: number, targetFrom: number, targetTo: number, easing: EasingType = "Linear") {
-        this.easings.push({
-            startTime,
-            endTime,
-            targetFrom,
-            targetTo,
-            easing,
-        });
+    // I hate TypeScript function overloading so much 🤮
+    addEasing(...easings: Easing[]): Easer;
+    addEasing(startTime: number, endTime: number, targetFrom: number, targetTo: number, easingType?: EasingType): Easer;
+    addEasing(
+        easingsOrStartTime: Easing | number,
+        endTime?: number | Easing,
+        targetFrom?: number | Easing,
+        targetTo?: number | Easing,
+        easingType: EasingType | Easing = "Linear"
+    ): Easer {
+        if (typeof easingsOrStartTime === "number") {
+            const startTime = easingsOrStartTime;
+            this.easings.push({
+                startTime: startTime as number,
+                endTime: endTime as number,
+                targetFrom: targetFrom as number,
+                targetTo: targetTo as number,
+                easingType: easingType as EasingType,
+            });
+        } else {
+            this.easings.push(...arguments);
+        }
 
         return this;
     }
+
+    removeEasing(...easings: Easing[]) {
+        this.easings = this.easings.filter((e) => !easings.includes(e));
+    }
+
+    removeAllEasings() {
+        this.easings = [];
+    }
 }
 
-export { Easer };
+export { Easer, Easing, EasingFunction, EasingType };
