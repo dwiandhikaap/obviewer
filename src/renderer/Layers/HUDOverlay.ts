c1c8b24ad5fed9538e14f5db2141fffb3efd56ee
Dwi Andhika <53227252+siveroo@users.noreply.github.com>
2022-07-05 19:06:26 +0700

    deleted old gameplay HUD code

--- src/renderer/Layers/HUDOverlay.ts
+++ src/renderer/Layers/HUDOverlay.ts
@@ -2,9 +2,6 @@ import { Application, Container } from "pixi.js";
 import { Beatmap } from "../../osu/Beatmap/Beatmap";
 import { GameHUD } from "../../osu/Graphics/HUD/GameHUD";
 import { getOsuPixelScale } from "../../util/osu-calculation";
-import { KeypressDrawable } from "../HUDOverlay/KeypressDrawable";
-import { HitResultDrawable } from "../HUDOverlay/HitResultDrawable";
-import { URBarDrawable } from "../HUDOverlay/URBarDrawable";
 
 interface HUDDrawProperty {
     resolution: [number, number];
@@ -12,10 +9,6 @@ interface HUDDrawProperty {
 }
 
 class HUDOverlay extends Container {
-    private hitOverlay: KeypressDrawable;
-    private hitResult: HitResultDrawable;
-    private urBar: URBarDrawable;
-
     private drawProperty: HUDDrawProperty = {} as HUDDrawProperty;
 
     constructor(private application: Application) {
@@ -24,31 +17,13 @@ class HUDOverlay extends Container {
             resolution: [this.application.view.width, this.application.view.height],
             scale: getOsuPixelScale(this.application.view.width, this.application.view.height),
         };
-
-        this.hitOverlay = new KeypressDrawable(this.drawProperty);
-        this.hitResult = new HitResultDrawable(this.drawProperty);
-        this.urBar = new URBarDrawable(this.drawProperty);
-
-        this.addChild(this.hitResult);
-        this.addChild(this.hitOverlay);
-        this.addChild(this.urBar);
     }
 
-    loadBeatmap(beatmap: Beatmap) {
-        this.urBar.difficulty = beatmap.difficulty;
-    }
+    loadBeatmap(beatmap: Beatmap) {}
 
-    loadHUD(gameHUD: GameHUD) {
-        this.urBar.bind(gameHUD.urBar);
-        this.hitOverlay.bind(gameHUD.keypressOverlay);
-        this.hitResult.bind(gameHUD.hitResultOverlay);
-    }
+    loadHUD(gameHUD: GameHUD) {}
 
-    draw(time: number) {
-        this.hitOverlay.draw(time);
-        this.hitResult.draw(time);
-        this.urBar.draw(time);
-    }
+    draw(time: number) {}
 }
 
 export { HUDOverlay, HUDDrawProperty };
