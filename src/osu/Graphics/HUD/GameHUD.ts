2fe31bb9375c7366ccb25c989e673166d3c372b6
Dwi Andhika <53227252+siveroo@users.noreply.github.com>
2022-07-06 17:04:26 +0700

    remove old game hud

--- src/osu/Graphics/HUD/GameHUD.ts
+++ src/osu/Graphics/HUD/GameHUD.ts
@@ -1,8 +1,4 @@
 import { Renderer } from "../../../renderer/Renderer";
-import { UnstableRate } from "../../Gameplay/UnstableRate";
-import { KeypressOverlay, KeypressHitInfo } from "./KeypressOverlay";
-import { HitResultInfo, HitResultOverlay } from "./HitResultOverlay";
-import { URBar } from "./URBar";
 
 class GameHUD {
     private _time: number;
@@ -11,34 +7,11 @@ class GameHUD {
     }
     public set time(time: number) {
         this._time = time;
-
-        this.keypressOverlay.time = time;
-        this.urBar.time = time;
     }
-
-    keypressOverlay: KeypressOverlay;
-    hitResultOverlay: HitResultOverlay;
-    urBar: URBar;
 
     constructor(private renderer: Renderer) {
-        this.keypressOverlay = new KeypressOverlay();
-        this.hitResultOverlay = new HitResultOverlay();
-        this.urBar = new URBar();
-
         renderer.loadHUD(this);
-    }
-
-    updateKeypressOverlay(keypressInfo: KeypressHitInfo) {
-        this.keypressOverlay.update(keypressInfo);
-    }
-
-    updateHitResultOverlay(hitResultInfo: HitResultInfo) {
-        this.hitResultOverlay.update(hitResultInfo);
-    }
-
-    updateUnstableRate(unstableRate: UnstableRate) {
-        this.urBar.update(unstableRate);
     }
 }
 
-export { GameHUD, KeypressOverlay, KeypressHitInfo, HitResultInfo };
+export { GameHUD };
